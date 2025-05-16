fae5c52ba3802964731eabf5c75c2c6339fb91b8
gitagitty <chenyu1056@gmail.com>
2025-05-16 13:45:23 +0800

    添加训练参数

--- yolotrain/train.py
+++ yolotrain/train.py
@@ -1,5 +1,13 @@
 from ultralytics import YOLO
 
-model = YOLO("path/to/your.pt")
+model = YOLO("/home/evan/tutorial-2024.10/yolotrain/yolov8s.pt")
 
-results = model.train(data="path/to/your/data.yaml", epochs=300, device=0, batch = 16)
+results = model.train(data="/home/evan/tutorial-2024.10/yolotrain/dc/data.yaml", 
+                      epochs=300, 
+                      device=0, 
+                      batch = 16
+                      resume=False, #是否继续训练
+                      project='runs/detect', # save_dir='project/name',
+                      name='train',
+                      )
+
